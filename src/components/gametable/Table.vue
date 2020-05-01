f2bd75b4a5ced90d370c6aeed26fe5d89fcefa26
jakub_izbicki <jakub.izbicki1@gmail.com>
2020-05-01 20:13:01 +0200

    Table grid wip

--- src/components/gametable/Table.vue
+++ src/components/gametable/Table.vue
@@ -1,28 +1,27 @@
 <template>
-  <div class="border-solid border-8">
+  <div>
     <transition-group name="grid" tag="div" class="grid text-vw">
       <div v-for="slot in gridSlots"
            :key="slot.id"
-           class="h-gridCard w-gridCard bg-gridSlot"
+           class="h-gridSlot w-full border-solid border-2 rounded-lg"
            :style="[{'grid-column-start': `${slot.col}`},
                     {'grid-row-start': `${slot.row}`}]"
            @dragover.prevent
            @dragenter="onDragEnter(slot.id)"
            @drop.prevent>
-        <div class="text-base">
-          {{slot.id}}
-        </div>
       </div>
 
       <div v-for="card in gridCards"
            :key="card.id"
-           class="h-gridCard w-gridCard bg-cardPlaceholder rounded-lg"
+           class="flex items-center justify-center"
            :style="[{'grid-column-start': `${card.col}`},
-                    {'grid-row-start': `${card.row}`}]"
-           draggable="true"
-           @dragstart="onDragStart(card.id)">
-        <div class="text-base">
-          {{card.name}}
+                    {'grid-row-start': `${card.row}`}]">
+        <div class=" absolute h-gridCard w-gridCard bg-cardPlaceholder rounded-lg"
+             draggable="true"
+             @dragstart="onDragStart(card.id)">
+          <div class="text-base">
+            {{card.name}}
+          </div>
         </div>
       </div>
     </transition-group>
@@ -59,8 +58,8 @@
     },
     computed: {
       gridSlots() {
-        const cols = 15;
-        const rows = 5;
+        const cols = 35;
+        const rows = 15;
         const slots = [];
 
         [...Array(cols).keys()].map(col => col + 1).forEach(col => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,14 @@ module.exports = {
     extend: {
       colors: {
         cardPlaceholder: "rgb(102,180,146)",
-        gridSlot: "rgb(207,214,220)",
       },
       height: {
         gridCard: `${10 * 0.88}em`,
+        gridSlot: `${2}em`,
       },
       width: {
         gridCard: `${10 * 0.63}em`,
+        gridSlot: `${5}em`,
         grid: '50em',
       },
       fontSize: {
