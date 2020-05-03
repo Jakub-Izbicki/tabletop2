439763cddc07ac342fb9dc1f4d6aa8e45dd59948
jakub_izbicki <jakub.izbicki1@gmail.com>
2020-05-03 22:55:23 +0200

    Start work on card prompt on grid drag hover

--- src/components/gametable/Table.vue
+++ src/components/gametable/Table.vue
@@ -3,12 +3,17 @@
     <transition-group name="grid" tag="div" class="grid text-vw">
       <div v-for="slot in gridSlots"
            :key="slot.id"
-           class="h-gridSlot w-full border-solid border-2 rounded-lg"
+           class="flex items-center justify-center
+                  h-gridSlot w-full border-solid border-2 rounded-lg"
            :style="[{'grid-column-start': `${slot.col}`},
                     {'grid-row-start': `${slot.row}`}]"
            @dragover.prevent
            @dragenter="onDragEnter(slot.id)"
+           @dragleave="onDragLeave"
            @drop.prevent="onDrop(slot.id)">
+<!--        <div ref=""-->
+<!--             class="absolute pointer-events-none h-gridCard w-gridCard bg-cardPrompt rounded-lg">-->
+<!--        </div>-->
       </div>
 
       <div v-for="card in gridCards"
@@ -35,6 +40,11 @@
 
   export default {
     name: "Table",
+    data() {
+      return {
+        dragHoveredSlotId: null,
+      }
+    },
     computed: {
       ...mapGetters(['isTableCardDrag']),
       ...createNamespacedHelpers('gameTable').mapState({
@@ -68,14 +78,20 @@
       onDragEnd() {
         this.$store.dispatch('resetDrag');
       },
-      onDragEnter() {
-
+      onDragEnter(slotId) {
+        this.dragHoveredSlotId = slotId;
+      },
+      onDragLeave() {
+        this.dragHoveredSlotId = null;
       },
       onDrop(slotId) {
         this.$store.dispatch('gameTable/moveCardToSlot', {
           cardId: this.draggedCardId,
           slotId,
         });
+      },
+      isDragHovered(slotId) {
+        return this.dragHoveredSlotId === slotId;
       }
     }
   }
