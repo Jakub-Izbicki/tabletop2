c6e2da693843af968ca08d9d705d6c784ae36675
Jakub Izbicki <jakub.izbicki1@gmail.com>
2020-05-13 17:15:20 +0200

    Init spring boot server

---
+++ tabletop-server/src/main/java/com/izbicki/jakub/tabletopserver/TabletopServerApplication.java
@@ -0,0 +1,13 @@
+package com.izbicki.jakub.tabletopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletopServerApplication.class, args);
+	}
+
+}
